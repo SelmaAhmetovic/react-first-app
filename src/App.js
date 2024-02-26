6b1cee170e4abacbeae146bdeca46ec7a5edd951
Selma Repuh <selma.repuh@endava.com>
2024-02-26 09:14:27 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
-function App() {
+const Person = (props) => {
+
+  return (
+    <>
+    <p>{props.name + " " + props.surname}</p>
+    <p>{props.age}</p>
+    </>
+  )
+}
+
+const ButtonComponent = () => {
+  // function starting with use is a hook
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    // counter = 100 BIG ERROR - never modify the state manually
+    setCounter(100);
+    // video 39min
+  }
+
+
+  )
+
+  return (
+    <>
+    <button onClick={() =>{setCounter(counter - 1)}}>-</button>
+    <h1>{counter}</h1>
+    <button  onClick={() =>{setCounter((nextCounter)=> nextCounter+1)}}>+</button>
+    
+    </>
+  )
+}
+
+const App = () => {
+  const name = 'Selma';
+  const isNameShowing = true;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Hello, {isNameShowing ? name: 'someone'}
+      </h1>
+
+      <div>
+        <Person name='Selma' age={20+9} surname={'Repuh'}/>
+      </div>
+      
+      {name ? (
+        <>
+          TEST
+        </>):(
+        <>
+          TEST 2
+        </>
+        )}
+
+        <div>
+          <ButtonComponent/>
+        </div>
     </div>
   );
 }
